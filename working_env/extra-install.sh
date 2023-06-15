dc7810b11496be75d20277c24b41b1c82a42bbaf
Jorge Bolinches <75072254+JorgeOP46n2@users.noreply.github.com>
2023-06-15 09:08:48 +0200

    Delete working_env directory
    Already in local_env directory

--- working_env/extra-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Enables and configures elasticsearch index
-bash /extra-install/ElasticSearch.sh
-
-# Creates an OAuth consumer for quickstatements
-bash /extra-install/QuickStatements.sh
